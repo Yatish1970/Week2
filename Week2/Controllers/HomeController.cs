a8ae787f7e874d3f921589b3bcce289400c0dfcf
827426 <827426@25-PC28>
2022-09-17 09:52:32 -0400

    Add project files.

---
+++ Week2/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Week2.Models;
+
+namespace Week2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public string Name()
+        {
+            return "Yatish Patel";
+        }
+
+
+        public string Email()
+        {
+            return "C0827426@mylabton.ca";
+        }
+
+        public IActionResult HtmlTester()
+        {
+            return View();
+        }
+    }
+}
